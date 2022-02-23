3ee3548b5ea232e5688b36e31678cd3273dd5798
Michael Fournier <michaeljfournier5@gmail.com>
2022-02-23 11:31:18 -0500

    commit tree e71e63e0de16172a8b5e322f8c736ddbc2314f3b

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(customer){
+    return Math.abs(customer - 42)
+}
+distanceFromHqInBlocks();
+
+function distanceFromHqInFeet(x){
+    let block = distanceFromHqInBlocks(x);
+    return block * 264
+}
+
+function distanceTravelledInFeet(start,destination){
+    return Math.abs((destination - start) * 264)
+}
+
+function calculatesFarePrice(start,destination){
+    let feet = distanceTravelledInFeet(start,destination);
+
+    if  (feet > 2500){
+        return 'cannot travel that far'
+    } else if (feet < 400){
+        return 0
+    }
+
+    let newFeet = feet-400
+
+    if (feet > 2000) {
+        return 25
+    } else {
+        return newFeet * .02
+    }
+}
